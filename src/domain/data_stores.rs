74eee0b9e6b1b811ea2e609c5c99d14aa8bafa4c
Mateusz Płaczek <mateusz.placzek@gmail.com>
2024-05-22 12:48:11 +0200

    implement delete for account

--- src/domain/data_stores.rs
+++ src/domain/data_stores.rs
@@ -14,4 +14,5 @@ pub trait UserStore: Send + Sync + 'static {
     async fn add_user(&mut self, user: User) -> Result<(), UserStoreError>;
     async fn get_user(self, email: &Email) -> Result<User, UserStoreError>;
     async fn validate_user(self, email: &Email, password: &Password) -> Result<(), UserStoreError>;
+    async fn delete_user(&mut self, email: &Email) -> Result<(), UserStoreError>;
 }
