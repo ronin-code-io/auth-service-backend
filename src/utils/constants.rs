0c0e3013d3412d0a80550f310e2a4af2ce1a3070
Mateusz Płaczek <mateusz.placzek@gmail.com>
2024-06-12 17:37:20 +0200

    Add postgres env variables, add wget

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -5,6 +5,7 @@ use std::env as std_env;
 lazy_static! {
     pub static ref JWT_SECRET: String = set_token();
     pub static ref ASSETS_DIR: String = set_assets_dir();
+    pub static ref POSTGRES_PASSWORD: String = set_postgres_password();
 }
 
 fn load_env_file() {
@@ -37,9 +38,21 @@ fn set_assets_dir() -> String {
     assets_dir
 }
 
+fn set_postgres_password() -> String {
+    load_env_file();
+    let postgres_password = std_env::var(env::POSTGRES_PASSWORD_ENV_VAR).unwrap_or_else(|_| {
+        panic!(
+            "{} environment variable must be set.",
+            env::POSTGRES_PASSWORD_ENV_VAR
+        )
+    });
+    postgres_password
+}
+
 pub mod env {
     pub const JWT_SECRET_ENV_VAR: &str = "JWT_SECRET";
     pub const ASSETS_DIR_ENV_VAR: &str = "ASSETS_DIR";
+    pub const POSTGRES_PASSWORD_ENV_VAR: &str = "POSTGRES_PASSWORD";
 }
 
 pub mod prod {
